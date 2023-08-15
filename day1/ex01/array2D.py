b8f2646f257d7f7da1a8714d19f927cec6e02e11
Avo Midoyan <avet.midoyan@gmail.com>
2023-08-15 15:35:31 +0400

    added gitignore and Day 1 ex01

---
+++ day1/ex01/array2D.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def slice_me(family: list, start: int, end: int) -> list:
+    """
+    A function takes parameters of 2D array, prints its shape, 
+    and returns a truncated version of the array.
+    param family: 2D array
+    param start:  start index
+    param end: end index
+    """
+    if not isinstance(family, list) or any(not isinstance(x, list)\
+            for x in family):
+        raise ValueError("family must be a list")
+    if not isinstance(start, int) or not isinstance(end, int):
+        raise ValueError("start must be an integer")
+    print(f"My shape is: {np.array(family).shape}")
+    ret_list = np.array(family[start:end])
+    print(f"New shape is: {ret_list.shape}")
+    return ret_list
