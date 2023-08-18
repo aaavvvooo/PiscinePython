499a15defa1c75411842d49412c2f70d84b62146
Avo Midoyan <avet.midoyan@gmail.com>
2023-08-19 02:45:03 +0400

    fixed norme error in Loading.py

--- day0/ex08/Loading.py
+++ day0/ex08/Loading.py
@@ -3,7 +3,8 @@ def ft_tqdm(lst: range) -> None:
     total = len(lst)
     length = 100
     for i in enumerate(lst):
-        percent = round(float(("{0:.1f}").format(100 * ((i[0] + 1) / float(total)))))
+        percent = round(float(("{0:.1f}").format(100 * ((i[0] + 1)
+                        / float(total)))))
         filledLength = int(length * (i[0] + 1) // total)
         bar = '█' * filledLength + ' ' * (length - filledLength)
         print(f'{percent}%|{bar}| {i[0] + 1}/{len(list((lst)))}', end='\r')
